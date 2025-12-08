3151f0e353c3695821d690ecd6019630a7f4af99
kenshgy <ken.shgy.1386@gmail.com>
2025-12-08 21:43:01 +0900

    Add TypeScript declarations for .mov file modules

--- front/src/components/InvitationContents.vue
+++ front/src/components/InvitationContents.vue
@@ -9,13 +9,10 @@ import CountDown from '@/components/CountDown.vue'
 import img1 from '@/assets/koma01.jpg'
 import img2 from '@/assets/koma02.jpg'
 import img3 from '@/assets/koma03.jpg'
-import img5 from '@/assets/od0257.jpg'
-import img7 from '@/assets/od0051.jpg'
-import img8 from '@/assets/od0241.jpg'
-import img9 from '@/assets/od0291.jpg'
+import topVideo from '@/assets/top.MOV'
 
-const topImages = [img5, img7, img9, img8]
 const middleImages = [img1, img2, img3]
+const heroVideoSrc = topVideo
 
 const props = defineProps({
   schedule: {
@@ -33,7 +30,16 @@ const props = defineProps({
   <v-card class="pa-4">
     <v-row>
       <v-col cols="12" lg="6" md="6" sm="12" xs="12">
-        <CarouselCard :image-list="topImages" />
+        <v-responsive aspect-ratio="16/9" class="rounded-lg overflow-hidden elevation-1">
+          <video
+            :src="heroVideoSrc"
+            autoplay
+            muted
+            loop
+            playsinline
+            style="width: 100%; height: 100%; object-fit: cover"
+          />
+        </v-responsive>
       </v-col>
       <v-col cols="12" lg="6" md="6" sm="12" xs="12">
         <MessageCard />
